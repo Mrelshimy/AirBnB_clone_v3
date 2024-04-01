d7123de7eb4ac8ad2a8a8ceaac7f0de695684458
Yahia <yn7599@gmail.com>
2024-04-01 14:54:04 +0200

    update to_dict password return

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, file_storage=False):
+    def to_dict(self, keep_pass=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,9 +68,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if file_storage is False:
-            if 'password' in new_dict:
-                del new_dict["password"]
+        if keep_pass:
+            new_dict.pop('password', 0)
         return new_dict
 
     def delete(self):
