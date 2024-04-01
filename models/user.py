cf25a207779f694fafd40159f8b29e65b79f6e8b
Yahia <yn7599@gmail.com>
2024-04-01 16:20:46 +0200

    password hashing

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -26,4 +27,15 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        password = kwargs.get('password')
+        if password and 'created_at' not in kwargs.keys():
+            hash = hashlib.md5(password.encode())
+            kwargs['password'] = hash.hexdigest()
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, key, val):
+        """hash the password before setting it"""
+        if key == 'password' and hasattr(self, 'password'):
+            hash = hashlib.md5(val.encode())
+            val = hash.hexdigest()
+        super(self.__class__, self).__setattr__(key, val)
